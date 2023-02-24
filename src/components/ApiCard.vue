292ee224522b2fda85dc7e399849970b67bb54f2
PaoloNapoli97 <paolonapoli@live.it>
2023-02-24 17:20:51 +0100

    Update Api Main info

--- src/components/ApiCard.vue
+++ src/components/ApiCard.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from "axios";
 import AppCard from "./AppCard.vue";
+import { store } from "../store";
 
 export default {
   components: {
@@ -8,21 +9,21 @@ export default {
   },
   data() {
     return {
-        apartments: [],
+      apartments: [],
+      store,
     };
   },
   created() {
     axios.get("http://localhost:8000/api/apartments").then((response) => {
       this.apartments = response.data;
+      this.store.apartments = response.data;
     });
   },
 };
 </script>
 
 <template>
-
-     <AppCard v-for="apartment in apartments" :data="apartment"/>
-
+  <AppCard v-for="apartment in apartments" :data="apartment" />
 </template>
 
 <style lang="scss" scoped></style>
--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="container">
-    <InfoHeader />
-    <SectionAparmentDescription />
+    <InfoHeader :data="store.apartments" />
+    <SectionAparmentDescription :data="store.apartments" />
   </div>
 </template>
 
 <script>
 import InfoHeader from "../components/apartment-components/InfoHeader.vue";
 import SectionAparmentDescription from "../components/apartment-components/SectionApartmentDescription.vue";
+import { store } from "../store";
 
 export default {
   components: {
     InfoHeader,
     SectionAparmentDescription,
+  },
+  data() {
+    return {
+      store,
+    };
   },
 };
 </script>
