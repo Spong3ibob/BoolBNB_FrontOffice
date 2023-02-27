a95218ae55e27ed83126dc943c58f04f115849e4
PaoloNapoli97 <paolonapoli@live.it>
2023-02-27 12:17:49 +0100

    Fix GetApartment Data Error

--- src/components/main-apartment-index/card-components/AppCard.vue
+++ src/components/main-apartment-index/card-components/AppCard.vue
@@ -8,19 +8,22 @@ export default {
   },
   props: {
     data: Object,
-  }
+  },
 };
 </script>
 
 <template>
-  <router-link :to="{ name: 'apartment-page', params:{ slug: data.slug } }" class="text-dark text-decoration-none mb-5" @click="this.getApartmentData(this.data)">
+  <router-link
+    :to="{ name: 'apartment-page', params: { slug: data.slug } }"
+    class="text-dark text-decoration-none mb-5"
+  >
     <div class="ms-card">
-      <img class="ms-card__image rounded-4 mb-3" :src="data.image" alt="">
-  
+      <img class="ms-card__image rounded-4 mb-3" :src="data.image_url" alt="" />
+
       <div class="ms-card__information">
         <h6 class="fw-bold">{{ data.title }}</h6>
-        <div class="text-muted">{{data.mq}} metri quadrati</div>
-        <div class="text-muted my-1">{{data.full_address}}</div>
+        <div class="text-muted">{{ data.mq }} metri quadrati</div>
+        <div class="text-muted my-1">{{ data.full_address }}</div>
         <div class="text-muted">
           <strong class="text-dark">{{ data.price }} €</strong> notte
         </div>
