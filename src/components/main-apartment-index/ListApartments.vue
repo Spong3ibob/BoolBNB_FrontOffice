03d0c383a168bd68ce74e7c1ae83aa2a0bed8a3b
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-02-27 18:24:53 +0100

    Gestito messaggio appartamenti non trovati

--- src/components/main-apartment-index/ListApartments.vue
+++ src/components/main-apartment-index/ListApartments.vue
@@ -15,11 +15,18 @@ export default {
 </script>
 
 <template>
-  <div class="ms-page-container d-flex flex-wrap mt-4 mb-5">
+  <div class="ms-page-container d-flex flex-wrap mt-4 mb-5" v-if="this.store.filteredApartments.length !== 0">
     <AppCard
       v-for="filteredApartment in this.store.filteredApartments"
       :data="filteredApartment"
     />
+  </div>
+  <div v-else>
+    <div class="ms-page-container d-flex justify-content-center align-items-center flex-wrap mt-5">
+      <div class="alert alert-danger mt-5">
+        <i class="fa-solid fa-ban fa-lg fa-fw"></i> Nessun appartamento trovato.
+      </div>
+    </div>
   </div>
 </template>
 
